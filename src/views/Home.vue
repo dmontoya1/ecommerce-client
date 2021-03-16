092d9ba293642eb50a632feed4b28e4af13425ba
Daniel Montoya <dmontoya.lab@gmail.com>
2021-03-15 19:47:01 -0500

    Layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <button class="ui primary button">
-      Save
-    </button>
+    <BasicLayout>
+      <h2>Estamos en la Home</h2>
+    </BasicLayout>
   </div>
 </template>
 
 <script>
+import BasicLayout from "./../Layouts/BasicLayout";
 export default {
   name: "Home",
+
+  components: {
+    BasicLayout,
+  },
 };
 </script>
