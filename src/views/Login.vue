49c8634a84af05d63e65b1246d90eccc7f13c5eb
Daniel Montoya <dmontoya.lab@gmail.com>
2021-03-16 12:12:28 -0500

    Login y Register

---
+++ src/views/Login.vue
@@ -0,0 +1,110 @@
+<template>
+  <BasicLayout>
+    <div class="login">
+      <h2>Iniciar sesión</h2>
+      <form class="ui form" @submit.prevent="login">
+        <div class="field">
+          <input
+            type="text"
+            placeholder="Nombre de usuario"
+            v-model="formData.identifier"
+            :class="{ error: formError.identifier }"
+          />
+        </div>
+        <div class="field">
+          <input
+            type="password"
+            placeholder="Contraseña"
+            v-model="formData.password"
+            :class="{ error: formError.password }"
+          />
+        </div>
+        <button
+          type="submit"
+          class="ui button fluid primary"
+          :class="{ loading }"
+        >
+          Iniciar Sesión
+        </button>
+      </form>
+      <router-link to="/register">Crear cuenta</router-link>
+    </div>
+  </BasicLayout>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import BasicLayout from "../Layouts/BasicLayout";
+import { loginApi } from "../api/user";
+import * as Yup from "yup";
+
+export default {
+  name: "Login",
+  components: {
+    BasicLayout,
+  },
+
+  setup() {
+    let formData = ref({});
+    let formError = ref({});
+    let loading = ref(false);
+    const router = useRouter();
+
+    const schemeForm = Yup.object().shape({
+      identifier: Yup.string().required(true),
+      password: Yup.string().required(true),
+    });
+
+    const login = async () => {
+      formError.value = {};
+      loading.value = true;
+      try {
+        await schemeForm.validate(formData.value, { abortEarly: false });
+        try {
+          const response = await loginApi(formData.value);
+          if (!response?.jwt) throw "El usuario o contraseña son inválidos";
+          console.log(response);
+          router.push("/");
+        } catch (error) {
+          console.log(error);
+        }
+      } catch (error) {
+        console.log(error);
+        error.inner.forEach((err) => {
+          formError.value[err.path] = err.message;
+        });
+      }
+
+      loading.value = false;
+    };
+
+    return {
+      formData,
+      formError,
+      login,
+      loading,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  text-align: center;
+  > h2 {
+    margin: 50px 0 30px 0;
+  }
+
+  .ui.form {
+    max-width: 300px !important;
+    margin: 0 auto;
+    margin-bottom: 10px;
+
+    input.error {
+      border-color: #faa;
+      background-color: #ffeded;
+    }
+  }
+}
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,117 @@
+<template>
+  <BasicLayout>
+    <div class="register">
+      <h2>Registro de usuario</h2>
+      <form class="ui form" @submit.prevent="register">
+        <div class="field">
+          <input
+            type="text"
+            placeholder="Nombre de usuario"
+            v-model="formData.username"
+            :class="{ error: formError.username }"
+          />
+        </div>
+        <div class="field">
+          <input
+            type="email"
+            placeholder="Correo electrónico"
+            v-model="formData.email"
+            :class="{ error: formError.email }"
+          />
+        </div>
+        <div class="field">
+          <input
+            type="password"
+            placeholder="Contraseña"
+            v-model="formData.password"
+            :class="{ error: formError.password }"
+          />
+        </div>
+        <button
+          type="submit"
+          class="ui button fluid primary"
+          :class="{ loading }"
+        >
+          Crear cuenta
+        </button>
+      </form>
+      <router-link class="link" to="/login">Iniciar sesión</router-link>
+    </div>
+  </BasicLayout>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import BasicLayout from "../Layouts/BasicLayout";
+import { registerApi } from "../api/user";
+import * as Yup from "yup";
+
+export default {
+  name: "Register",
+  components: {
+    BasicLayout,
+  },
+  setup() {
+    let formData = ref({});
+    let formError = ref({});
+    let loading = ref(false);
+    const router = useRouter();
+
+    const schemeForm = Yup.object().shape({
+      username: Yup.string().required(true),
+      email: Yup.string()
+        .email(true)
+        .required(true),
+      password: Yup.string().required(true),
+    });
+
+    const register = async () => {
+      formError.value = {};
+      loading.value = true;
+
+      try {
+        await schemeForm.validate(formData.value, { abortEarly: false });
+        try {
+          const response = await registerApi(formData.value);
+          router.push("/login");
+        } catch (error) {
+          console.log(error);
+        }
+      } catch (error) {
+        error.inner.forEach((err) => {
+          formError.value[err.path] = err.message;
+        });
+      }
+
+      loading.value = false;
+    };
+
+    return {
+      formData,
+      formError,
+      register,
+      loading,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.register {
+  text-align: center;
+  > h2 {
+    margin: 50px 0 30px 0;
+  }
+
+  .ui.form {
+    max-width: 300px !important;
+    margin: 0 auto;
+    margin-bottom: 10px;
+    input.error {
+      border-color: #faa;
+      background-color: #ffeded;
+    }
+  }
+}
+</style>
