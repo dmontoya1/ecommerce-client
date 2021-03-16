20f828ef09dc8902696ea5f091f4040ad6b078a3
Daniel Montoya <dmontoya.lab@gmail.com>
2021-03-15 19:39:59 -0500

    Integrando Semantic UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <button class="ui primary button">
+      Save
+    </button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: "Home",
 };
